449df5b5b130840bd59a272924f2bc7cdc3916c0
Oscar Bonilla <obonilla@linkedin.com>
2017-05-11 15:55:16 -0700

    Initial config

---
+++ init.el
@@ -0,0 +1,11 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-exec-path)
+(require 'init-ui)
+(require 'init-editing)
+(require 'init-navigation)
+(require 'init-miscellaneous)
+(require 'init-company-mode)
+
+(provide 'init)
---
+++ lisp/init-exec-path.el
@@ -0,0 +1,7 @@
+(require 'init-elpa)
+
+(require-package 'exec-path-from-shell)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(provide 'init-exec-path)
