a44c6457c8b86e18588271b6f1ace59fb55dfb44
Oscar Bonilla <6f6231@gmail.com>
2017-11-13 21:16:15 -0800

    Trying inconsolata once again
    * This should really be conditioned on window-system

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,4 +1,12 @@
+;;; ob -- my fonts
+;;; Commentary:
+;;; Code:
+
+;; (set-frame-font
+;;  "-*-Hack-normal-normal-normal-*-11-*-*-*-m-0-iso10646-1" nil t)
+
 (set-frame-font
-  "-*-Hack-normal-normal-normal-*-11-*-*-*-m-0-iso10646-1" nil t)
+  "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil t)
+
 (provide 'init-fonts)
 ;;; init-fonts ends here
