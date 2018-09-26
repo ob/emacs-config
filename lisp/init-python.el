4eee6ba6b55213a24c7d80caf12731787ba5ee23
Oscar Bonilla <obonilla@linkedin.com>
2018-09-26 14:43:11 -0700

    Add missing python file

---
+++ lisp/init-python.el
@@ -0,0 +1,13 @@
+;;; ruby -- init-python.el
+;;; Commentary:
+;;; Code:
+
+(require 'init-elpa)
+(require-package 'better-defaults)
+(require-package 'elpy)
+(require-package 'flycheck)
+
+(elpy-enable)
+
+(provide 'init-python)
+;;; init-python ends here
