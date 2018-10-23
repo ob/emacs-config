0194345d274491053f1490253ebb55d97cff4e83
Oscar Bonilla <obonilla@linkedin.com>
2018-10-22 22:03:32 -0700

    Add .inputrc

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 (require 'init-company-mode)
 (require 'init-git)
 (require 'init-ediff)
+(require 'init-jira)
 (require 'init-ob)
 
 ;; Languages
@@ -50,13 +51,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-day)))
  '(custom-safe-themes
    (quote
-    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-selected-packages
    (quote
-    (go-add-tags go-autocomplete go-complete go-direx go-dlv go-eldoc go-errcheck go-fill-struct go-gen-test go-gopath go-guru go-impl go-imports go-mode go-playground go-playground-cli go-projectile go-rename go-scratch go-snippets go-stacktracer go-tag jinja2-mode bazel-mode cmake-font-lock cmake-ide cmake-mode cmake-project cpputils-cmake toml-mode better-defaults company-sourcekit yaml-mode markdown-mode markdown-mode+ magit sanityinc-tomorrow-night flycheck-rust racer company projectile smex ido-ubiquitous flycheck rainbow-delimiters golden-ratio atom-one-dark-theme exec-path-from-shell)))
+    (helm-jira jira-markup-mode org-jira ox-jira atom-one-light-theme enh-ruby-mode go-add-tags go-autocomplete go-complete go-direx go-dlv go-eldoc go-errcheck go-fill-struct go-gen-test go-gopath go-guru go-impl go-imports go-mode go-playground go-playground-cli go-projectile go-rename go-scratch go-snippets go-stacktracer go-tag jinja2-mode bazel-mode cmake-font-lock cmake-ide cmake-mode cmake-project cpputils-cmake toml-mode better-defaults company-sourcekit yaml-mode markdown-mode markdown-mode+ magit sanityinc-tomorrow-night flycheck-rust racer company projectile smex ido-ubiquitous flycheck rainbow-delimiters golden-ratio atom-one-dark-theme exec-path-from-shell)))
  '(safe-local-variable-values
    (quote
     ((flycheck-clang-language-standard . c++14)
