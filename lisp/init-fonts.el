f6ca61ad04f468486703e68075e927322b144c5a
Oscar Bonilla <6f6231@gmail.com>
2023-06-16 11:53:48 -0700

    Some git aliases and font update

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -5,13 +5,13 @@
 ;; (set-frame-font
 ;;  "-*-Hack-normal-normal-normal-*-11-*-*-*-m-0-iso10646-1" nil t)
 
-(set-frame-font
- "-*-Fira Code-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1" nil t)
+;; (set-frame-font
+;;  "-*-Fira Code-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1" nil t)
 
 (if (or (eq window-system 'ns) (eq window-system 'mac))
-    ;; (set-frame-font
-    ;;  "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil t)
-  (mac-auto-operator-composition-mode))
+    (set-frame-font
+     "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1" nil t)
+  (mac-auto-operator-composition-mode))
 
 (provide 'init-fonts)
 ;;; init-fonts ends here
--- transient/history.el
+++ transient/history.el
@@ -1,4 +1,8 @@
 ((magit-commit nil)
  (magit-diff
   ("--no-ext-diff" "--stat"))
- (magit-dispatch nil))
+ (magit-dispatch nil)
+ (magit-ediff nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-push nil))
